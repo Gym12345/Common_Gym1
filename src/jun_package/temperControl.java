e3d1b7e0a7c6b6aafea5c1ca291860c47c0b62dd
gym <bondal3333@gmail.com>
2024-02-16 17:07:20 +0900

    minor change1

--- src/jun_package/temperControl.java
+++ src/jun_package/temperControl.java
@@ -1,18 +1,24 @@
 package jun_package;
+import java.util.Scanner;
 
 public class temperControl {
-	package jun_package;
 
-	import java.util.Scanner;
 
-	public class temper() {
+	
 		Scanner input=new Scanner(System.in);
+		
+		
 		public void temperControl(){
+			
+			
 			System.out.println("어떤 온도에 맞춰드릴까요??");
 		
 			int select=input.nextInt();
 			
 			int temper=20;
+			
+			
+			
 			while(true) {
 				System.out.println("현재 온도는 :"+temper+"도입니다.");
 				System.out.println("온도를 올리시려면 1번을 내리시려면 2번을 눌러주세요.");
@@ -25,6 +31,7 @@ public class temperControl {
 					System.out.println("잘못입력하였습니다.");
 				}
 			}
+			
 		}
 	}
-}
+
